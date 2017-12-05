a23e31f37b0cdb65e16c38c702366ff9343d5758
Rebecca Stockbridge <rstockbridge.dev@gmail.com>
2017-12-05 08:14:11 -0500

    Minor changes to Day 2
    Add use of final.

--- day02/src/main/java/Main.java
+++ day02/src/main/java/Main.java
@@ -10,34 +10,34 @@ import java.util.stream.Collectors;
 final class Main {
 
     public static void main(String[] args) {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/Users/rebecca/Desktop/Dropbox/documents/coding/AdventOfCode2017/day02/src/main/java/input.txt"))) {
-            List<List<Integer>> storedInputFile = new ArrayList<>();
+        try (final BufferedReader bufferedReader = new BufferedReader(new FileReader("/Users/rebecca/Desktop/Dropbox/documents/coding/AdventOfCode2017/day02/src/main/java/input.txt"))) {
+            final List<List<Integer>> storedInputFile = new ArrayList<>();
 
             for (String line; (line = bufferedReader.readLine()) != null; ) {
                 storedInputFile.add(Arrays.stream(line.split("\t")).map(Integer::valueOf).collect(Collectors.toList()));
             }
 
-            System.out.format("The checksum in Part I is %d.\n", calculatePartIChecksum(storedInputFile));
-            System.out.format("The checksum in Part II is %d.\n", calculatePartIIChecksum(storedInputFile));
+            System.out.format("Part I: the checksum is %d.\n", calculatePartIChecksum(storedInputFile));
+            System.out.format("Part II: the checksum is %d.\n", calculatePartIIChecksum(storedInputFile));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int calculatePartIChecksum(List<List<Integer>> storedInputFile) {
+    private static int calculatePartIChecksum(final List<List<Integer>> storedInputFile) {
         int checksum = 0;
 
-        for (List<Integer> lineValues : storedInputFile) {
+        for (final List<Integer> lineValues : storedInputFile) {
             checksum += Collections.max(lineValues) - Collections.min(lineValues);
         }
 
         return checksum;
     }
 
-    private static int calculatePartIIChecksum(List<List<Integer>> storedInputFile) {
+    private static int calculatePartIIChecksum(final List<List<Integer>> storedInputFile) {
         int checksum = 0;
 
-        for (List<Integer> values : storedInputFile) {
+        for (final List<Integer> values : storedInputFile) {
             for (int i = 0; i < values.size() - 1; i++) {
                 for (int j = i + 1; j < values.size(); j++) {
                     if (values.get(i) % values.get(j) == 0) {
